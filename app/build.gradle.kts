4b0af07939c002ae863ecdd15eb47445317d0b82
Alban Dericbourg <alban@dericbourg.dev>
2026-02-27 17:22:11 +0100

    Release version 1.0.7
    Signed-off-by: Alban Dericbourg <alban@dericbourg.dev>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.dericbourg.firstclassmetronome"
         minSdk = 29
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
 
         testInstrumentationRunner = "dev.dericbourg.firstclassmetronome.HiltTestRunner"
     }
